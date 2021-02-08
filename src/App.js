8ee3e5f5daa9ea0443446d691a3f67fe1b851387
millord <juancarlosmillord.gmail.com>
2021-02-08 12:59:44 -0400

    add styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./switcher.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="theme-options">
+        <div id="theme-white" />
+        <div id="theme-blue" />
+        <div id="theme-orange" />
+        <div id="theme-purple" />
+        <div id="theme-green" />
+        <div id="theme-black" />
+      </div>
+      <div className="content-box">
+        <h3>Multiple Theme Switcher / React</h3>
+        <h5>(React.js, SCSS/CSS, Local Storage)</h5>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mattis
+          ex ac arcu maximus tristique. Pellentesque at nibh at leo venenatis
+          iaculis eu ut libero. Sed tincidunt faucibus lorem, vel aliquam felis
+          tempor id. Donec ipsum enim, mollis sed mauris vel, egestas fringilla
+          nibh. Praesent facilisis, eros sit amet tincidunt sollicitudin, tortor
+          est mollis elit, at volutpat ante lorem at elit. Vestibulum ultricies
+          arcu justo, a ornare ex vehicula id. Duis lectus elit, egestas eu
+          velit vel, aliquet suscipit dui. Fusce quis quam ut nibh luctus
+          consectetur. Duis nunc ante, vestibulum at elit non, tincidunt aliquet
+          est. Ut convallis viverra justo, ut efficitur risus interdum sed.
+          Aliquam iaculis dolor sit amet ullamcorper finibus. Mauris sit amet
+          dolor non mi volutpat tristique id et velit. Interdum et malesuada
+          fames ac ante ipsum primis in faucibus.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>-- &#923; juan</p>
+      </div>
     </div>
   );
 }
